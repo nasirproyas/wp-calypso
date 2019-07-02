a74d7d7b0b446da35b7250e94eb9ed32151476c2
Payton Swick <payton@foolord.com>
2019-07-02 18:14:15 -0400

    StripeElementsPaymentBox: Render a CardElement

--- client/my-sites/checkout/checkout/stripe-elements-payment-box.jsx
+++ client/my-sites/checkout/checkout/stripe-elements-payment-box.jsx
@@ -5,15 +5,19 @@
  */
 import { loadScript } from '@automattic/load-script';
 import debugFactory from 'debug';
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { localize } from 'i18n-calypso';
+import { StripeProvider, Elements, injectStripe, CardElement } from 'react-stripe-elements';
 
 const debug = debugFactory( 'calypso:stripe-elements-payment-box' );
 
 // TODO: move this to somewhere else
 const stripeJsUrl = 'https://js.stripe.com/v3/';
+// TODO: move this to somewhere else
+const stripeApiKey = 'pk_test_12345';
 
-function useStripeJs( url ) {
+function useStripeJs( url, apiKey ) {
+	const [ stripeJs, setStripeJs ] = useState( null );
 	useEffect( () => {
 		if ( window.Stripe ) {
 			debug( 'stripe.js already loaded' );
@@ -26,13 +30,34 @@ function useStripeJs( url ) {
 				return;
 			}
 			debug( 'stripe.js loaded!' );
+			setStripeJs( window.Stripe( apiKey ) );
 		} );
-	}, [ url ] );
+	}, [ url, apiKey ] );
+	return stripeJs;
 }
 
+function StripeElementsForm() {
+	/* eslint-disable jsx-a11y/label-has-associated-control */
+	return (
+		<label>
+			Card details
+			<CardElement />
+		</label>
+	);
+	/* eslint-enable jsx-a11y/label-has-associated-control */
+}
+
+const InjectedStripeElementsForm = injectStripe( StripeElementsForm );
+
 export function StripeElementsPaymentBox() {
-	useStripeJs( stripeJsUrl );
-	return <em>Stripe stuff here</em>;
+	const stripeJs = useStripeJs( stripeJsUrl, stripeApiKey );
+	return (
+		<StripeProvider stripe={ stripeJs }>
+			<Elements>
+				<InjectedStripeElementsForm />
+			</Elements>
+		</StripeProvider>
+	);
 }
 
 export default localize( StripeElementsPaymentBox );
