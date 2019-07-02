0655b6a658d294bc9d3d32863f11dc0069367216
Payton Swick <payton@foolord.com>
2019-07-02 17:33:43 -0400

    Add placeholder StripeElementsPaymentBox

---
+++ client/my-sites/checkout/checkout/stripe-elements-payment-box.jsx
@@ -0,0 +1,18 @@
+/** @format */
+
+/**
+ * External dependencies
+ */
+
+import React from 'react';
+import { localize } from 'i18n-calypso';
+
+/**
+ * Internal dependencies
+ */
+
+export function StripeElementsPaymentBox() {
+	return <em>Stripe stuff here</em>;
+}
+
+export default localize( StripeElementsPaymentBox );
