515bb5be0205cf863bd8e661d413d572e219e16d
Sérgio Gomes <mail@sgomes.com>
2019-07-02 12:57:44 +0100

    Record build target served.
    We currently don't keep track of how many requests get served with the
    fallback vs the evergreen target. This change adds a count for each.
    
    To achieve this, it also adds the ability to record counting-type stats.

--- client/lib/analytics/test/statsd.js
+++ client/lib/analytics/test/statsd.js
@@ -1,5 +1,3 @@
-/** @format */
-
 /**
  * External dependencies
  */
@@ -9,7 +7,7 @@ import url from 'url';
 /**
  * Internal dependencies
  */
-import { statsdTimingUrl } from '../statsd';
+import { statsdTimingUrl, statsdCountingUrl } from '../statsd';
 
 describe( 'StatsD Analytics', () => {
 	describe( 'statsdTimingUrl', () => {
@@ -20,6 +18,23 @@ describe( 'StatsD Analytics', () => {
 			expect( sdUrl.query.json ).to.eql(
 				JSON.stringify( {
 					beacons: [ 'calypso.development.post_mysite_com.page_load:150|ms' ],
+				} )
+			);
+		} );
+	} );
+
+	describe( 'statsdCountingUrl', () => {
+		test( 'returns a URL for recording counting data to statsd', () => {
+			const sdUrl = url.parse(
+				statsdCountingUrl( 'post-mysite.com', 'page-count', 1 ),
+				true,
+				true
+			);
+			expect( sdUrl.query.v ).to.eql( 'calypso' );
+			expect( sdUrl.query.u ).to.eql( 'post_mysite_com' );
+			expect( sdUrl.query.json ).to.eql(
+				JSON.stringify( {
+					beacons: [ 'calypso.development.post_mysite_com.page_count:1|c' ],
 				} )
 			);
 		} );
