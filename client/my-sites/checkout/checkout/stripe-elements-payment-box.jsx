807ba692469a1100865e21390f0cdb1668149d0d
Payton Swick <payton@foolord.com>
2019-07-02 17:58:38 -0400

    Load stripe.js in StripeElementsPaymentBox

--- client/my-sites/checkout/checkout/stripe-elements-payment-box.jsx
+++ client/my-sites/checkout/checkout/stripe-elements-payment-box.jsx
@@ -3,15 +3,35 @@
 /**
  * External dependencies
  */
-
-import React from 'react';
+import { loadScript } from '@automattic/load-script';
+import debugFactory from 'debug';
+import React, { useEffect } from 'react';
 import { localize } from 'i18n-calypso';
 
-/**
- * Internal dependencies
- */
+const debug = debugFactory( 'calypso:stripe-elements-payment-box' );
+
+// TODO: move this to somewhere else
+const stripeJsUrl = 'https://js.stripe.com/v3/';
+
+function useStripeJs( url ) {
+	useEffect( () => {
+		if ( window.Stripe ) {
+			debug( 'stripe.js already loaded' );
+			return;
+		}
+		debug( 'loading stripe.js...' );
+		loadScript( url, function( error ) {
+			if ( error ) {
+				debug( 'stripe.js script ' + error.src + ' failed to load.' );
+				return;
+			}
+			debug( 'stripe.js loaded!' );
+		} );
+	}, [ url ] );
+}
 
 export function StripeElementsPaymentBox() {
+	useStripeJs( stripeJsUrl );
 	return <em>Stripe stuff here</em>;
 }
 
